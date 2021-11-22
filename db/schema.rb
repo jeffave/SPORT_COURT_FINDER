dd6bb61daa280defa9a2179b64fa036ead2e0a75
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-22 18:03:05 -0300

    created models and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_22_203849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "courts", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "availability"
+    t.float "pricing"
+    t.integer "capacity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_courts_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "date"
+    t.bigint "user_id", null: false
+    t.bigint "court_id", null: false
+    t.boolean "accepted"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["court_id"], name: "index_reservations_on_court_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.string "birthday"
+    t.string "phone"
+    t.string "languages"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "courts", "users"
+  add_foreign_key "reservations", "courts"
+  add_foreign_key "reservations", "users"
 end
