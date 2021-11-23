3edccd22acae441e4ca13af4b510c636eef45334
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-23 14:08:13 -0300

    fixed the court form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_142720) do
+ActiveRecord::Schema.define(version: 2021_11_23_150306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_142720) do
   create_table "courts", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.string "availability"
+    t.boolean "availability"
     t.float "pricing"
     t.integer "capacity"
     t.bigint "user_id", null: false
