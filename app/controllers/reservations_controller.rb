ced3e37dd7d17c26e9e4dc8cd89a5fef9db355d7
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-23 17:30:20 -0300

    Added reservation controller actions to courts#show

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,27 @@
 class ReservationsController < ApplicationController
 
-  def new
-    @reservation = Reservation.new
+  def create
+    @court = Court.find(params[:court_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    @reservation.court = @court
+    if @reservation.save!
+      redirect_to court_path(@court)
+    else
+      redirect_to courts_path
+    end
   end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:date)
+  end
+
 
 end
