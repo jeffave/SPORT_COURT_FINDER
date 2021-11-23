ccc8cc3c5951ea903441c55c70292e3afc4f9190
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-23 11:30:44 -0300

    remove columns from users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  # This is our landing page
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # We're using all our routes for courts
+  resources :courts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_203849) do
+ActiveRecord::Schema.define(version: 2021_11_23_142720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,11 +40,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_203849) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
-    t.string "last_name"
-    t.string "gender"
-    t.string "birthday"
-    t.string "phone"
-    t.string "languages"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
