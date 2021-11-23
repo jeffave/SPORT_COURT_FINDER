2e1421de3a0fcbf21b61f8907411f93847111280
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-23 11:44:52 -0300

    removed custom validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,6 @@
 class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :courts, through: :reservations
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :gender, presence: true
-  validates :birthday, presence: true
-  validates :phone, presence: true
-  validates :languages, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
