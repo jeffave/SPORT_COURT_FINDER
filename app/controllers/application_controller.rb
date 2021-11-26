f70528e6ec2420476b04f20c331849bd5413b033
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-26 16:30:15 -0300

    validating model and fixing button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    user_path(current_user) # your path
+    user_path(current_user)
   end
 
   private
--- app/models/court.rb
+++ app/models/court.rb
@@ -2,7 +2,7 @@ class Court < ApplicationRecord
   has_one_attached :photo
   has_many :reservations
   belongs_to :user
-  validates :name, :location, :pricing, :capacity, presence: true
+  validates :name, :location, :pricing, :capacity, :photo, presence: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
